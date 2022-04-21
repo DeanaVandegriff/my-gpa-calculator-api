8a51e6d83c46225d9014e572d26ce392296e9047
deana <dvandegriff1@gmail.com>
2022-04-21 14:14:06 -0500

    seeds users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# User.create!(name: "Zack Morris", email: "zackmorris@example.com", password: "bayside")
+
+User.create!(name: "Kelly Kapowski", email: "kellykapowski@example.com", password: "bayside")
+
+User.create!(name: "A.C. Slater", email: "acslater@example.com", password: "bayside")
+
+User.create!(name: "Jessie Spano", email: "jessiespano@example.com", password: "bayside")
+
+User.create!(name: "Screech Powers", email: "screechpowers@example.com", password: "bayside")
+
+User.create!(name: "Lisa Turtle", email: "lisaturtle@example.com", password: "bayside")
+
+User.create!(name: "Richard Belding", email: "theprincipal@example.com", password: "bayside")
