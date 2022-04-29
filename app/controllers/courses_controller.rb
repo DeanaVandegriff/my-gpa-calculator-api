d01cc11e9ba0f13e8bc2c69594a2660408f07004
deana <dvandegriff1@gmail.com>
2022-04-29 14:31:34 -0500

    adds create, update and destroy actions

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -3,4 +3,34 @@ class CoursesController < ApplicationController
     courses = Course.all
     render json: courses.as_json
   end
+
+  def create
+    course = Course.new(
+      name: params[:name],
+      grade: params[:grade],
+      credits: params[:credits],
+      level: params[:level],
+      semester_taken: params[:semester_taken],
+      user_id: current_user.id,
+    )
+    course.save
+    render json: course.as_json
+  end
+
+  def update
+    course = Course.find_by(id: params[:id])
+    course.name = params[:name] || course.name
+    course.grade = params[:grade] || course.grade
+    course.credits = params[:credits] || course.credits
+    course.level = params[:level] || course.level
+    course.semester_taken = params[:semester_taken] || course.semester_taken
+    course.save
+    render json: course.as_json
+  end
+
+  def destroy
+    course = Course.find_by(id: params[:id])
+    course.destroy
+    render json: { message: "course removed" }
+  end
 end
