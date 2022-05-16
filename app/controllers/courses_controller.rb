2602370de151f016596f9fae48a187b8a9164db9
deana <dvandegriff1@gmail.com>
2022-05-16 12:18:44 -0500

    updates application controller to normal mode

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -2,12 +2,6 @@ class CoursesController < ApplicationController
   before_action :authenticate_user
 
   def index
-    # @courses.each do
-    #   total_point_value += @point_value
-    # end
-    # grade_point_average = total_point_value / @point_value.size
-    # puts grade_point_average
-    # grade_point_average = current_user.courses.sum(:point_value) / current_user.courses.count
     if current_user
       @courses = current_user.courses
     else
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,8 +1,4 @@
 class Course < ApplicationRecord
   belongs_to :user
-
-  # def grade_point_average
-  #   grade_point_average = current_user.courses.sum(:point_value) / current_user.courses.count
-  # end
 end
 
